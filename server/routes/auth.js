f40f4544fa787aeb71f4c0b08f3ec59ec97ba144
thaison247 <nguyenthaison247@gmail.com>
2021-07-11 00:34:20 +0700

    Added login api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -35,7 +35,43 @@ router.post('/register', async (req, res) => {
         return res.json({success: true, message: 'User created successfully', accessToken});
 
     } catch(error){
+        console.log(error);
+        res.status(500).json({success: false, message: 'Internal server error'});
+    }
+});
 
+
+// @route POST api/auth/login
+// @desc Login user
+// @access Public
+router.post('/login', async(req, res)=>{
+    const {username, password} = req.body;
+
+    // simple validation
+    if (!username || !password) {
+        return res.status(400).json({success: false, message: 'Missing username and/or password'});
+    }
+
+    try {
+        // check for existing user
+        const user = await User.findOne({username: username});
+        if (!user) {
+            return res.status(400).json({success: false, message: 'Incorrect username or password'});
+        }
+
+        // username found
+        const passwordValid = await argon2.verify(user.password, password);
+        if(!passwordValid) {
+            return res.status(400).json({success: false, message: 'Incorrect username or password'}); 
+        }
+
+        // all good
+        // return token
+        const accessToken = jwt.sign({userId: user._id}, process.env.ACCESS_TOKEN_SECRET);
+        return res.json({success: true, message: 'Logged in successfully', accessToken});
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({success: false, message: 'Internal server error'});
     }
 })
 
