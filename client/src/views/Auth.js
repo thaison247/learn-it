5dcf22e626a6f9aa7bb815b7cc72c4bc84a0a590
thaison247 <nguyenthaison247@gmail.com>
2021-07-13 23:13:25 +0700

    Init client. Auth routing.

---
+++ client/src/views/Auth.js
@@ -0,0 +1,28 @@
+import React from "react";
+import LoginForm from "../components/auth/LoginForm";
+import RegisterForm from "../components/auth/RegisterForm";
+
+function Auth({ authRoute }) {
+  let body;
+  body = (
+    <>
+      LearnIT
+      {authRoute === "login" && <LoginForm />}
+      {authRoute === "register" && <RegisterForm />}
+    </>
+  );
+
+  return (
+    <div className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1>LearnIt</h1>
+          <h4>Keep track of what you are learning</h4>
+          {body}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Auth;
