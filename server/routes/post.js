579246e9e60c953f7f18a6ef342128a3cd86286d
thaison247 <nguyenthaison247@gmail.com>
2021-07-13 20:47:19 +0700

    Added api delete post

--- server/routes/post.js
+++ server/routes/post.js
@@ -96,4 +96,29 @@ router.put("/:id", verifyToken, async (req, res) => {
   }
 });
 
+// @route DELETE api/posts/:id
+// @desc Delete post
+// @access Private
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    const postDeleteCondition = { _id: req.params.id, user: req.userId };
+    const deletedPost = await Post.findOneAndDelete(postDeleteCondition);
+
+    // User not authorized or post not found
+    if (!deletedPost) {
+      return res.status(401).json({
+        success: false,
+        message: "Post not found or user not authorized",
+      });
+    }
+
+    return res.json({
+      success: true,
+      post: deletedPost,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
 module.exports = router;
