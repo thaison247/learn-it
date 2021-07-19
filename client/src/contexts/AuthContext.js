d8b55ffa0177713756d0d315a744e3ae32a795dc
thaison247 <nguyenthaison247@gmail.com>
2021-07-19 23:55:01 +0700

    Client: checking user is authenticated in AuthComponent

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,7 +1,8 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import axios from "axios";
 import { authReducer } from "../reducers/authReducer";
 import { apiUrl, LOCAL_STORAGE_TOKEN_NAME } from "./constant";
+import setAuthToken from "../utils/setAuthToken";
 
 export const AuthContext = createContext();
 
@@ -11,6 +12,32 @@ const AuthContextProvider = ({ children }) => {
     isAuthenticated: false,
     user: null,
   });
+
+  // Authenticate user
+  const loadUser = async () => {
+    if (localStorage[LOCAL_STORAGE_TOKEN_NAME]) {
+      setAuthToken(localStorage[LOCAL_STORAGE_TOKEN_NAME]);
+    }
+
+    try {
+      const response = await axios.get(`${apiUrl}/auth`);
+      if (response.data.success) {
+        dispatch({
+          type: "SET_AUTH",
+          payload: { isAuthenticated: true, user: response.data.user },
+        });
+      }
+    } catch (error) {
+      localStorage.removeItem(LOCAL_STORAGE_TOKEN_NAME);
+      setAuthToken(null);
+      dispatch({
+        type: "SET_AUTH",
+        payload: { isAuthenticated: false, user: null },
+      });
+    }
+  };
+
+  useEffect(() => loadUser(), []);
 
   // Login
   const loginUser = async (userForm) => {
@@ -31,7 +58,7 @@ const AuthContextProvider = ({ children }) => {
   };
 
   // Context data
-  const authContextData = { loginUser };
+  const authContextData = { loginUser, authState };
 
   // Return provider
   return (
--- client/src/views/Auth.js
+++ client/src/views/Auth.js
@@ -1,15 +1,33 @@
 import React from "react";
 import LoginForm from "../components/auth/LoginForm";
 import RegisterForm from "../components/auth/RegisterForm";
+import { AuthContext } from "../contexts/AuthContext";
+import { useContext } from "react";
+import { Redirect } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
 
 function Auth({ authRoute }) {
+  const {
+    authState: { authLoading, isAuthenticated },
+  } = useContext(AuthContext);
+
   let body;
-  body = (
-    <>
-      {authRoute === "login" && <LoginForm />}
-      {authRoute === "register" && <RegisterForm />}
-    </>
-  );
+  if (authLoading) {
+    body = (
+      <div className="d-flex justify-content-center mt-2">
+        <Spinner animation="border" variant="info" />
+      </div>
+    );
+  } else if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  } else {
+    body = (
+      <>
+        {authRoute === "login" && <LoginForm />}
+        {authRoute === "register" && <RegisterForm />}
+      </>
+    );
+  }
 
   return (
     <div className="landing">
